610584faf245fe33950aadc274e9a79c4e566af6
brian.spilman <brian.spilman@wsu.edu>
2023-03-01 11:51:53 -0800

    finished commit

--- src/Main/Board.java
+++ src/Main/Board.java
@@ -6,16 +6,23 @@ public class Board {
 
     // should instantiate the board to be a 3x3 array:
     public Board() {
-
+        this.board = new char[3][3];
     }
 
     // this method should take in a row, column, and a newChar and update the board accordingly:
     public void updateBoard(int row, int column, char newChar) {
-
+        this.board[row][column] = newChar;
     }
 
     // this method should return a string representation of the board:
     public String toString() {
-        return "";
+        String boardRepresentation = "";
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++) {
+                boardRepresentation = boardRepresentation + this.board[i][j] + " ";
+            }
+            boardRepresentation += "\n";
+        }
+        return boardRepresentation;
     }
 }
--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.util.Scanner;
+
 enum Status {
     // there is a row, column or diagonal consisting of only player1's game pieces:
     PLAYER1_WON,
@@ -19,17 +21,92 @@ public class Game {
     // this function should prompt the players for names and gamePiece and assign it
     // to the player objects:
     private void initPlayers() {
-        return;
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Player 1 enter name: ");
+        String p1Name = scan.nextLine();
+        System.out.print("Player 1 enter game piece: ");
+        char p1GamePiece = scan.nextLine().charAt(0);
+        System.out.print("Player 2 enter name: ");
+        String p2Name = scan.nextLine();
+        System.out.print("Player 2 enter game piece: ");
+        char p2GamePiece = scan.nextLine().charAt(0);
+
+        this.player1 = new Player(p1Name, p1GamePiece);
+        this.player2 = new Player(p2Name, p2GamePiece);
     }
 
     // this method should let each player pick a spot and also print the board between selections:
-    private void round() {
+    private void round(boolean turn) {
+        Scanner scan = new Scanner(System.in);
+        if(!turn) {
+            System.out.print("Player 1 select row: ");
+            int p1Row = scan.nextInt();
+            System.out.print("Player 1 select column: ");
+            int p1Column = scan.nextInt();
+            this.board.updateBoard(p1Row, p1Column, this.player1.getGamePiece());
+            System.out.println(this.board.toString());
+        }
+        else {
+            System.out.print("Player 2 select row: ");
+            int p2Row = scan.nextInt();
+            System.out.print("Player 2 select column: ");
+            int p2Column = scan.nextInt();
+            this.board.updateBoard(p2Row, p2Column, this.player2.getGamePiece());
+            System.out.println(this.board.toString());
+        }
 
     }
 
     // This should return a status of the board, using the enum Status:
     private Status getStatus() {
-        return Status.UNFINISHED;
+        char p1 = player1.getGamePiece();
+
+        if((this.board.board[0][0] == this.board.board[0][1]) && (this.board.board[0][0] == this.board.board[0][2]) && (this.board.board[0][0] != 0)){
+            if(p1 == this.board.board[0][0]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[1][0] == this.board.board[1][1]) && (this.board.board[1][0] == this.board.board[1][2]) && (this.board.board[1][0] != 0)){
+            if(p1 == this.board.board[1][0]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[2][0] == this.board.board[2][1]) && (this.board.board[2][0] == this.board.board[2][2]) && (this.board.board[2][0] != 0)){
+            if(p1 == this.board.board[2][0]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[0][0] == this.board.board[1][0]) && (this.board.board[0][0] == this.board.board[2][0]) && (this.board.board[0][0] != 0)){
+            if(p1 == this.board.board[0][0]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[0][1] == this.board.board[1][1]) && (this.board.board[0][1] == this.board.board[2][1]) && (this.board.board[0][1] != 0)){
+            if(p1 == this.board.board[0][1]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[0][2] == this.board.board[1][2]) && (this.board.board[0][2] == this.board.board[2][2]) && (this.board.board[0][2] != 0)){
+            if(p1 == this.board.board[0][2]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[0][0] == this.board.board[1][1]) && (this.board.board[0][0] == this.board.board[2][2]) && (this.board.board[0][0] != 0)){
+            if(p1 == this.board.board[0][0]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+        if((this.board.board[2][0] == this.board.board[1][1]) && (this.board.board[2][0] == this.board.board[0][2]) && (this.board.board[2][0] != 0)){
+            if(p1 == this.board.board[2][0]) return Status.PLAYER1_WON;
+            else return Status.PLAYER2_WON;
+        }
+
+        boolean tie = true;
+
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++) {
+                if(this.board.board[i][j] == 0) {
+                    tie = false;
+                    break;
+                }
+            }
+        }
+
+        if(tie) return Status.TIE;
+        else return Status.UNFINISHED;
 
     }
 
@@ -38,7 +115,25 @@ public class Game {
     // 3. Print the board after each move:
     // 4. When the game is over, announce the winner or that it is a tie
     public static void main(String[] args) {
+
+        Game game = new Game();
+        game.initPlayers();
+        game.board = new Board();
+        System.out.println(game.board.toString());
+        boolean turn = false;
+
+        while(game.getStatus() == Status.UNFINISHED){
+            game.round(turn);
+            if(turn) turn = false;
+            else turn = true;
+        }
+
+        if(game.getStatus() == Status.PLAYER1_WON){
+            System.out.println("Congrats " + game.player1.getName() + " on winning!!");
+        }
+        else if(game.getStatus() == Status.PLAYER2_WON){
+            System.out.println("Congrats " + game.player2.getName() + " on winning!!");
+        }
+        else System.out.println("Tie game! Better luck next time!");
     }
-
-
 }
